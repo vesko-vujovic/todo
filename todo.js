b00a1aa06fa1bd1e1ff18963ae97c5ce3f0b4ef7
Vesko Vujovic <veskovujovic@yahoo.com>
2015-04-21 18:42:56 +0200

    error
    i forgot to switch variables

--- todo.js
+++ todo.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
    	   globalObj = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
 
 
-   	   $.cookie("vesko", parsedObj);
+   	   $.cookie("vesko", globalObj);
 
    }
 
