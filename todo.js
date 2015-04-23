4f29a13d0d34a8884905b27d1d65ca28020a66b1
Vesko Vujovic <veskovujovic@yahoo.com>
2015-04-24 00:10:11 +0200

    Thanks for playin!
    Everything works

--- todo.js
+++ todo.js
@@ -4,11 +4,7 @@ $(document).ready(function(){
     var list       = $('#list');
     var arrayOfObj = [];
     var obj;
-
-     var txt = $('#list li').text();
-    alert(''+ txt +'');
-
-
+    
    //general function that will call other functions
    function addListElement(event)
    {
@@ -33,12 +29,14 @@ $(document).ready(function(){
         obj.value = ''+ input +'';
         addObjectToArray(obj);
     }
+
     //function to push this object
     function addObjectToArray(obj)
     {
         arrayOfObj.push(obj);
         convertToJson(arrayOfObj);
     }
+
     //convert that array of objects to json
     function convertToJson(arrObj)
     {
@@ -46,18 +44,21 @@ $(document).ready(function(){
         addToCookie(convert);
 
     }
+
     //add converted json array of objects to cookie
     function addToCookie(converted)
     {
         var cook = $.cookie('tasks', converted)
         parseCookie();
     }
+
     // parse cookie with parseJSON
     function parseCookie()
     {
         var parsedCookie = $.parseJSON($.cookie('tasks'));
         readParsedCookie(parsedCookie);
     }
+
     //read parsed cookie
     function readParsedCookie(parsedCookie)
     {
@@ -65,11 +66,6 @@ $(document).ready(function(){
            list.after('<li id="member"><input type="checkbox">' + lastOne.value + '<button class="delete">Delete</button></li>');
 
     }
-
-
-
-
-
        //event for invoking a function that calls other functions
        $('#add').on('click', addListElement);
        //event for deleting a node
@@ -80,6 +76,14 @@ $(document).ready(function(){
        $('#clear').on('click', function(){
           $('input:checked').parent().remove();
        });
+
+    // this function will display everythig after page loads
+    $(function() {
+        var parse = $.parseJSON($.cookie('tasks'));
+        $.each(parse, function(index, value){
+           list.after('<li id="member"><input type="checkbox">' + value.value + '<button class="delete">Delete</button></li>');
+        });
+    });
 
 
 });
